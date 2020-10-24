4851554614a6f2512d1e2782ebfdfd1051018dc6
Martin <castello.martin@yahoo.com.ar>
2020-10-24 02:11:18 -0300

    Test 6 heroku

--- src/server.js
+++ src/server.js
@@ -6,10 +6,10 @@ const path = require('path');
 const app = express();   
 
 // Serve only the static files form the dist directory    
-app.use(express.static(__dirname + '/src'));
+app.use(express.static(__dirname + '/dist/out-tsc'));
 
 app.get('/*', function(req,res) {  
-    res.sendFile(path.join(__dirname+'/src/index.html'));   
+    res.sendFile(path.join(__dirname+'/dist/out-tsc/index.html'));   
 });  
 
 // Start the app by listening on the default Heroku port    
